01ebc69e2c56224377127dc59386b923f946e170
SFotakos <s.fotakos@gmail.com>
2018-05-08 09:56:08 -0300

    Implements Step 3

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,21 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-import com.sfotakos.joketellinglib.Jokester;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.sfotakos.puppeteer.StageActivity;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,10 +48,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
+        new JokeBackendAsyncTask().execute(this);
+    }
+
+    public void displayJoke(String joke){
         Intent jokeActivityIntent = new Intent(this, StageActivity.class);
-        jokeActivityIntent.putExtra(StageActivity.JOKE_STAGE, Jokester.entertainUs());
+        jokeActivityIntent.putExtra(StageActivity.JOKE_STAGE, joke);
         startActivity(jokeActivityIntent);
     }
 
+    // Implemented as seen on https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/77e9910911d5412e5efede5fa681ec105a0f02ad/HelloEndpoints#2-connecting-your-android-app-to-the-backend
+    class JokeBackendAsyncTask extends AsyncTask<MainActivity, Void, String>{
+        private String LOCAL_HOST = "http://10.0.2.2:8080";
+        private MyApi myApiService = null;
+        private MainActivity activity;
 
+        @Override
+        protected String doInBackground(MainActivity... activities) {
+            if(myApiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl(LOCAL_HOST + "/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+
+                myApiService = builder.build();
+            }
+
+            activity = activities[0];
+            try {
+                return myApiService.getJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            activity.displayJoke(result);
+        }
+    }
 }
