5bc6772d72bfcfaa84f9fb812b24693a6981c7b1
SFotakos <s.fotakos@gmail.com>
2018-05-09 12:08:08 -0300

    Removes ad related string from main string.xml and properly waits for AsyncTask to finish before ending test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTest.java
@@ -5,6 +5,9 @@ import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.core.IsNot.not;
@@ -16,13 +19,20 @@ public class JokeTest {
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void getJoke(){
+    public void getJoke() throws InterruptedException {
+        // Implemented CountDownLatch to wait for AsyncTask as shown in
+        // https://stackoverflow.com/a/5722193  (09/05/2018)
+        final CountDownLatch signal = new CountDownLatch(1);
+
         JokeBackendAsyncTask jokeBackendAsyncTask = new JokeBackendAsyncTask();
         jokeBackendAsyncTask.execute(new JokeBackendAsyncTask.IAPICallback() {
             @Override
             public void displayJoke(String joke) {
                 assertThat(joke, not(isEmptyOrNullString()));
+                signal.countDown();
             }
         });
+
+        signal.await(30, TimeUnit.SECONDS);
     }
 }
